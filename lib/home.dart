121514a7343421838c5740cf639230e15e037d84
nandeeshkamath <nandeeshkamath99@gmail.com>
2023-11-23 22:50:36 +0530

    Update home screen

--- lib/home.dart
+++ lib/home.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:wing_cook/add_ingredient.dart';
 import 'package:wing_cook/add_recipe.dart';
+import 'package:wing_cook/constants/app_theme.dart';
 import 'package:wing_cook/create_estimate.dart';
 import 'package:wing_cook/database/ingredients_repository.dart';
-import 'package:wing_cook/database/recipe_ingredients_map_repository.dart';
 import 'package:wing_cook/database/recipe_repository.dart';
 import 'package:wing_cook/view_ingredients.dart';
 import 'package:wing_cook/view_recipes.dart';
@@ -14,38 +14,40 @@ class HomePage extends StatelessWidget {
   Widget gridTile(
       BuildContext context, title, Color color, Widget? onTap, IconData icon) {
     return Material(
-      elevation: 8,
-      borderRadius: BorderRadius.circular(10),
-      color: Colors.blue,
+      elevation: 20,
+      // borderRadius: BorderRadius.circular(10),
+      shape: RoundedRectangleBorder(
+        side: const BorderSide(color: primary),
+        borderRadius: BorderRadius.circular(25),
+      ),
+      // color: Colors.lightBlue[100],
       child: TextButton(
-          onPressed: () {
-            if (onTap != null) {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => onTap),
-              );
-            }
-          },
-          style: ButtonStyle(
-            foregroundColor: MaterialStateProperty.all(Colors.black),
-            overlayColor: MaterialStateProperty.all(Colors.transparent),
+        onPressed: () {
+          if (onTap != null) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => onTap),
+            );
+          }
+        },
+        style: ButtonStyle(
+          foregroundColor: MaterialStateProperty.all(Colors.black),
+          overlayColor: MaterialStateProperty.all(Colors.transparent),
+        ),
+        child: GridTile(
+          footer: Text(
+            title,
+            textAlign: TextAlign.center,
+            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+            // style: Theme.of(context).textTheme.headlineSmall,
           ),
-          child: GridTile(
-            footer: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 5),
-              child: Text(
-                title,
-                textAlign: TextAlign.center,
-                style:
-                    const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-                // style: Theme.of(context).textTheme.headlineSmall,
-              ),
-            ),
-            child: Icon(
-              icon,
-              size: 70,
-            ),
-          )),
+          child: Icon(
+            icon,
+            color: Colors.indigo[500],
+            size: 70,
+          ),
+        ),
+      ),
     );
   }
 
@@ -54,23 +56,68 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: const Text('Paaka Mitra'),
+        title: const Text(
+          'Paaka Mitra',
+          style: TextStyle(fontWeight: FontWeight.w500, letterSpacing: 1.1),
+        ),
+        backgroundColor: primary,
+        elevation: 20,
+        toolbarHeight: 150,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(20),
+            bottomRight: Radius.circular(20),
+          ),
+        ),
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(0),
+          child: SizedBox(
+            width: double.infinity,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 30),
+              child: Material(
+                elevation: 20,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50)),
+                child: TextField(
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    hintText: "Search",
+                    contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 13),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50),
+                      // borderSide: const BorderSide(color: primary),
+                    ),
+                    isDense: true,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
         actions: [
           IconButton(
             onPressed: () {
               IngredientsRepository.deleteAll();
               RecipesRepository.deleteAll();
             },
-            icon: const Icon(Icons.delete_forever),
+            icon: const Icon(
+              Icons.delete_forever,
+              color: Colors.black,
+            ),
           ),
           IconButton(
-            icon: const Icon(Icons.person),
+            icon: const Icon(
+              Icons.person,
+              color: Colors.black,
+            ),
             tooltip: 'Open profile',
             onPressed: () {},
           ),
         ],
       ),
-      backgroundColor: Colors.white,
       drawer: const Drawer(
         child: Column(
           children: [
@@ -113,26 +160,37 @@ class HomePage extends StatelessWidget {
           ],
         ),
       ),
-      body: Center(
-        child: GridView.count(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          shrinkWrap: true,
-          crossAxisCount: 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
+      body: SingleChildScrollView(
+        child: Column(
           children: [
-            gridTile(context, 'Create estimate', Colors.orange,
-                const CreateEstimation(), Icons.calculate),
-            gridTile(
-                context, 'View estimates', Colors.orange, null, Icons.history),
-            gridTile(context, 'Add ingredient', Colors.orange,
-                const AddIngredient(), Icons.app_registration),
-            gridTile(context, 'View ingredients', Colors.orange,
-                const ViewIngredients(), Icons.restaurant),
-            gridTile(context, 'Add recipe', Colors.orange, const AddRecipe(),
-                Icons.add_task),
-            gridTile(context, 'View recipes', Colors.orange,
-                const ViewRecipes(), Icons.table_restaurant),
+            const SizedBox(
+              height: 30,
+            ),
+            SingleChildScrollView(
+              child: GridView.count(
+                // scrollDirection: Axis.vertical,
+                physics: const ScrollPhysics(),
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                shrinkWrap: true,
+                crossAxisCount: 2,
+                crossAxisSpacing: 20,
+                mainAxisSpacing: 20,
+                children: [
+                  gridTile(context, 'Create estimate', Colors.orange,
+                      const CreateEstimation(), Icons.calculate),
+                  gridTile(context, 'View estimates', Colors.orange, null,
+                      Icons.history),
+                  gridTile(context, 'Add ingredient', Colors.orange,
+                      const AddIngredient(), Icons.app_registration),
+                  gridTile(context, 'View ingredients', Colors.orange,
+                      const ViewIngredients(), Icons.restaurant),
+                  gridTile(context, 'Add recipe', Colors.orange,
+                      const AddRecipe(), Icons.add_task),
+                  gridTile(context, 'View recipes', Colors.orange,
+                      const ViewRecipes(), Icons.table_restaurant),
+                ],
+              ),
+            )
           ],
         ),
       ),
